4e1afded8a73f96b2ed9e897620b19d10cdd2b1a
Jim Nguyen <jimnguyen220@gmail.com>
2021-01-16 15:39:56 -0600

    updated var insertDate to use moment.js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,4 @@
 //inserts current date
-var insertDate = $("#currentDay").text(GetCurrentDate());
+var insertDate = $("#currentDay").text(moment().format("MMM Do YY"));
 
-function GetCurrentDate() {
-    var tDate = new Date();
-    var dd = tDate.getDate();
-    var MM = tDate.getMonth();
-    var yyyy = tDate.getFullYear();
-    var currentDate = (MM+1)  + "-" + dd + "-" + yyyy;
-    return currentDate;
-};
 
-//schedule

fd2a9f4eb6e3f49ef4c9912b4443b95b46506878
Jim Nguyen <jimnguyen220@gmail.com>
2021-01-16 22:19:47 -0600

    named variables for time and commented out logic to update color

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # dayPlanner
 
-The goal of this project is to create a scheduler to add events to a daily planner.  These events will be stored to the local storage where we can set and get the information. Some starter code is already given.  A screen shot of how it displays is below: 
+The goal of this project is to create a scheduler to add events to a daily planner.  These events will be stored to the local storage where we can set and get the information. The calendar is also color coded to update with the current time. If the time has passed, the text box will be greyed out, the present time will be red, and future time is green.  Some starter code is already given.  A screen shot of how it displays is below: 
 
 ![Before Image](./assets/schedulerBefore.jpg)
 
@@ -20,3 +20,10 @@ This is what the page looks like after it is finished:
 Github repository: https://github.com/jimnguyen220/dayPlanner
 
 Live website: https://jimnguyen220.github.io/dayPlanner/
+
+Resources:
+
+https://getbootstrap.com/
+
+https://momentjs.com/
+
--- javascript.js
+++ javascript.js
@@ -1,4 +1,59 @@
 //inserts current date
-var insertDate = $("#currentDay").text(moment().format("MMM Do YY"));
+var insertDate = $("#currentDay")
 
+insertDate.text(moment().format('LL'));
+
+//daily planner code here
+var timeNine = $("#nine");
+var timeTen = $("#ten");
+var timeEleven = $("#eleven");
+var timeTwelve = $("#twelve");
+var timeOne = $("#one");
+var timeTwo = $("#two");
+var timeThree = $("#three");
+var timeFour = $("#four");
+var timeFive = $("#five");
+
+
+
+// var currentTime = $(".form-control");
+// var hour = new Date().getHours();
+// if (hour >= 9 && hour <= 10) {
+//     currentTime.addClass("present");
+// } else if(hour >= 10 && hour <=11) {
+//     currentTime.addClass("present");
+// } else if (hour >= 11 && hour <= 12) {
+//     currentTime.addClass("present");
+// } else if (hour >= 12 && hour <= 1){
+//     currentTime.addClass("present");
+// } else if (hour >= 1 && hour <= 2){
+//     currentTime.addClass("present");
+// } else if (hour >= 2 && hour <= 3){
+//     currentTime.addClass("present");
+// } else if (hour >= 3 && hour <= 4){
+//     currentTime.addClass("present");
+// }else if (hour >= 4 && hour <= 5){
+//     currentTime.addClass("present");
+// }else {
+//     if (hour <= 9 && hour <10) {
+//         currentTime.addClass("future");
+//     }
+//     else if (hour <= 10 && hour <11) {
+//         currentTime.addClass("future");
+//     } else if (hour <= 11 && hour <12) {
+//             currentTime.addClass("future");
+//     } else if (hour <= 12 && hour <1) {
+//             currentTime.addClass("future");
+//     } else if (hour <= 1 && hour <2) {
+//         currentTime.addClass("future");
+//     } else if (hour <= 2 && hour <3) {
+//         currentTime.addClass("future");
+//     } else if (hour <= 3 && hour <4) {
+//         currentTime.addClass("future");
+//     } else if (hour <= 4 && hour <5) {
+//         currentTime.addClass("future");
+//     }else {
+//         currentTime.addClass("past");
+//     }
+// }
 

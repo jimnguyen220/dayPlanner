82d12a16f6f22e878477ec0b0ee82c0a1397453c
Jim Nguyen <jimnguyen220@gmail.com>
2021-01-18 18:09:33 -0600

    long hand set storage and get storage functions work

--- javascript.js
+++ javascript.js
@@ -4,17 +4,73 @@ var insertDate = $("#currentDay")
 insertDate.text(moment().format('LL'));
 
 //daily planner code here
-var textNine = document.getElementsByName("nine").value;
+
 var btnNine = document.getElementById("btnNine");
+var saveBtn = document.querySelector(".saveBtn");
+var nineEl = document.querySelector("#nine");
+var tenEl = document.querySelector("#ten");
+var elevenEl = document.querySelector("#eleven");
+var twelveEl = document.querySelector("#twelve");
+var oneEl = document.querySelector("#one");
+var twoEl = document.querySelector("#two");
+var threeEl = document.querySelector("#three");
+var fourEl = document.querySelector("#four");
+var fiveEl = document.querySelector("#five");
 
 
-var nineText = localStorage.getItem("nineText");
+function renderContent () {
+    var nineText = localStorage.getItem("nineText");
+    var tenText = localStorage.getItem("tenText");
+    var elevenText = localStorage.getItem("elevenText");
+    var twelveText = localStorage.getItem("twelveText");
+    var oneText = localStorage.getItem("oneText");
+    var twoText = localStorage.getItem("twoText");
+    var threeText = localStorage.getItem("threeText");
+    var fourText = localStorage.getItem("fourText");
+    var fiveText = localStorage.getItem("fiveText");
 
-textNine.textContent = nineText;
+    nineEl.setAttribute("value", nineText);
+    tenEl.setAttribute("value", tenText);
+    elevenEl.setAttribute("value", elevenText);
+    twelveEl.setAttribute("value", twelveText);
+    oneEl.setAttribute("value", oneText);
+    twoEl.setAttribute("value", twoText);
+    threeEl.setAttribute("value", threeText);
+    fourEl.setAttribute("value", fourText);
+    fiveEl.setAttribute("value", fiveText);
 
-btnNine.addEventListener("click", function() {
-    
+}
+
+renderContent();
+
+// btnNine.addEventListener("click", function() {
+//     var nineText = document.getElementById("nine").value;
+//     localStorage.setItem("nineText", nineText);
+//     var tenText = document.getElementById("ten").value;
+//     localStorage.setItem("tenText", tenText);
+
+// });
+
+saveBtn.addEventListener("click", function() {
+    var nineText = document.getElementById("nine").value;
+    var tenText = document.getElementById("ten").value;
+    var elevenText = document.getElementById("eleven").value;
+    var twelveText = document.getElementById("twelve").value;
+    var oneText = document.getElementById("one").value;
+    var twoText = document.getElementById("two").value;
+    var threeText = document.getElementById("three").value;
+    var fourText = document.getElementById("four").value;
+    var fiveText = document.getElementById("five").value;
+
     localStorage.setItem("nineText", nineText);
+    localStorage.setItem("tenText", tenText);
+    localStorage.setItem("elevenText", elevenText);
+    localStorage.setItem("twelveText", twelveText);
+    localStorage.setItem("oneText", oneText);
+    localStorage.setItem("twoText", twoText);
+    localStorage.setItem("threeText", threeText);
+    localStorage.setItem("fourText", fourText);
+    localStorage.setItem("fiveText", fiveText);
 });
 
 
@@ -78,3 +134,34 @@ btnNine.addEventListener("click", function() {
 //     }
 // }
 
+
+
+
+
+
+// var nineEl = document.getElementsByName("nine");
+// var btnNine = document.getElementById("btnNine");
+
+// function saveLastContent() {
+//     var scheduleContent = {
+//         nineText: nineEl.value,
+//         // tenText = tenEl.value,
+//     }
+//     localStorage.setItem("scheduleContent", JSON.stringify(scheduleContent));
+// };
+
+// function renderLastContent (){
+
+//     // var lastContent = JSON.parse(localstorage.getItem("scheduleContent"));
+//     var lastContent = JSON.parse(localStorage.getItem("scheduleContent"));
+//     if (lastContent !== null) {
+//         document.getElementsByName("nine") = scheduleContent.nineText;
+//     } else{
+//         return;
+//     }
+// }
+// btnNine.addEventListener("click", function(event){
+//     event.preventDefault();
+//     saveLastContent();
+//     renderLastContent ()
+// })
